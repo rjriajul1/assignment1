324656985acc4ef2111f5cb6bffc319ac1fc03cb
Riajul Islam <rjriajul1>
2025-11-17 07:41:24 +0600

    I solved problem 1 to 5 now

--- dist/solution.js
+++ dist/solution.js
@@ -1,3 +1,50 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const formatValue = (value) => {
+    if (typeof value === 'string') {
+        return value.toUpperCase();
+    }
+    else if (typeof value === 'number') {
+        return value * 10;
+    }
+    else {
+        return !value;
+    }
+};
+const getLength = (value) => {
+    if (typeof value === 'string') {
+        return value.length;
+    }
+    else if (Array.isArray(value)) {
+        return value.length;
+    }
+    return 0;
+};
+class Person {
+    name;
+    age;
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    getDetails() {
+        return (`'Name: ${this.name}, Age: ${this.age}'`);
+    }
+}
+const filterByRating = (items) => {
+    return items.filter((item) => item.rating >= 4);
+};
+const products = [
+    { title: 'Book A', rating: 5 },
+    { title: 'Book B', rating: 3.2 },
+    { title: 'Book C', rating: 5.0 }
+];
+const filterActiveUsers = (users) => {
+    return users.filter((user) => user.isActive === true);
+};
+const users = [
+    { id: 1, name: 'Rakib', email: 'rakib@example.com', isActive: true },
+    { id: 2, name: 'Asha', email: 'asha@example.com', isActive: false },
+    { id: 3, name: 'Rumi', email: 'rumi@example.com', isActive: true },
+];
 //# sourceMappingURL=solution.js.map
---
+++ src/solution.ts
@@ -0,0 +1,70 @@
+
+const formatValue = <T extends string | number | boolean> (value: T): string | number | boolean => {
+     if(typeof value === 'string'){
+      return value.toUpperCase();
+     }else if (typeof value === 'number'){
+        return value * 10 
+     }else{
+        return !value
+     }
+}
+
+
+const getLength = <T extends string | any[]> (value: T): number => {
+         if(typeof value === 'string'){
+           return value.length;
+         }else if(Array.isArray(value)) {
+            return value.length;
+         }
+         return 0;
+}
+
+
+class Person {
+   name: string;
+   age: number;
+   constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age
+   }
+   getDetails(){
+      return (`'Name: ${this.name}, Age: ${this.age}'`)
+   }
+}
+
+type Item = {
+    title: string;
+    rating: number;
+}
+
+const filterByRating = (items: Item[]): Item[] => {
+   return items.filter((item)=>item.rating >= 4)
+}
+
+const products: Item[] = [
+   {title: 'Book A', rating: 5},
+   {title: 'Book B', rating: 3.2},
+   {title: 'Book C', rating: 5.0}
+]
+
+
+
+type User = {
+   id: number;
+   name: string;
+   email: string;
+   isActive: boolean;
+}
+
+const filterActiveUsers = (users: User[]):User[] => {
+  return users.filter((user)=> user.isActive === true)
+}
+
+const users: User[] = [
+   {id:1, name: 'Rakib', email: 'rakib@example.com', isActive: true},
+   {id:2, name: 'Asha', email: 'asha@example.com', isActive: false},
+   {id:3, name: 'Rumi', email: 'rumi@example.com', isActive: true},
+]
+
+
+
